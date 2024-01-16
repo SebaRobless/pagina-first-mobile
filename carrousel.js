026c69c934a3bf59db70edf952d185d7fdeab89a
sebarobles <sebarob1021@gmail.com>
2024-01-16 18:30:09 -0300

    version carrusel sin actualizar

--- carrousel.js
+++ carrousel.js
@@ -1,6 +1,6 @@
 const flechaDerecha = document.querySelector(".container-buton-right");
 let indiceImagenActual = 0; // variable que indica el indice de la imagen en la que se encuentra el carrusel
-
+let transitionButton=false;// se usara para que el boton que permite cambiar imagenes no sea clickeable mientras ocurre una transicion
 const divCarrousel = document.querySelector(".carrousel-imgs");
 const imagenesCarrousel = divCarrousel.querySelectorAll("img"); 
 const arrayImagenes = Array.from(imagenesCarrousel); 
@@ -8,49 +8,66 @@ const arrayImagenes = Array.from(imagenesCarrousel);
 console.log(arrayImagenes);
 
 
-// let intervaloCarrusel = setInterval(siguienteImagen, 5000); 
+// Manejar evento de visibilidad
+document.addEventListener("visibilitychange", function() {
+    if (document.visibilityState === "hidden") {
+        clearInterval(intervaloCarrusel); // Pausar el carrusel cuando la pestaña esté en segundo plano
+    } else if (document.visibilityState === "visible") {
+        // Reanudar el carrusel cuando la pestaña vuelva a estar en primer plano
+        intervaloCarrusel = setInterval(siguienteImagen, 5000);
+    }
+});
+
+
+
+
+ let intervaloCarrusel = setInterval(siguienteImagen, 5000); 
 //la funcion set interval permite llamar a una funcion automaticamente en un lapso de tiempo indicado en el segundo parametro
 
 
 flechaDerecha.addEventListener("click",siguienteImagen);
 
 function siguienteImagen(){
-    
-
-    if(indiceImagenActual<(arrayImagenes.length-1)){
-        if(indiceImagenActual==0){
-        arrayImagenes[indiceImagenActual+1].style.transform = "translateX(0)";
-        arrayImagenes[indiceImagenActual+1].style.zIndex = "10";
-        arrayImagenes[indiceImagenActual].style.transform = "translateX(-100%)";
-        arrayImagenes[indiceImagenActual].style.zIndex = "10";
-        arrayImagenes[arrayImagenes.length-1].style.transform = "translateX(100%)";
-        arrayImagenes[arrayImagenes.length-1].style.zIndex = "5";
-        indiceImagenActual++;
+    if(!transitionButton){
+        transitionButton = true;
+        if(indiceImagenActual<(arrayImagenes.length-1)){
+            if(indiceImagenActual==0){
+                arrayImagenes[indiceImagenActual+1].style.transform = "translateX(0)";
+                arrayImagenes[indiceImagenActual+1].style.zIndex = "10";
+                arrayImagenes[indiceImagenActual].style.transform = "translateX(-100%)";
+                arrayImagenes[indiceImagenActual].style.zIndex = "10";
+                arrayImagenes[arrayImagenes.length-1].style.transform = "translateX(100%)";
+                arrayImagenes[arrayImagenes.length-1].style.zIndex = "5";
+                indiceImagenActual++;
+            }
+            else{
+                arrayImagenes[indiceImagenActual+1].style.transform = "translateX(0)";
+                arrayImagenes[indiceImagenActual+1].style.zIndex = "10";
+                arrayImagenes[indiceImagenActual].style.transform = "translateX(-100%)";
+                arrayImagenes[indiceImagenActual].style.zIndex = "10";
+                arrayImagenes[indiceImagenActual-1].style.transform = "translateX(100%)";
+                arrayImagenes[indiceImagenActual-1].style.zIndex = "5";
+                indiceImagenActual++;
+            }
         }
-        else{
-            arrayImagenes[indiceImagenActual+1].style.transform = "translateX(0)";
-            arrayImagenes[indiceImagenActual+1].style.zIndex = "10";
+        else if (indiceImagenActual==(arrayImagenes.length-1)){
             arrayImagenes[indiceImagenActual].style.transform = "translateX(-100%)";
             arrayImagenes[indiceImagenActual].style.zIndex = "10";
             arrayImagenes[indiceImagenActual-1].style.transform = "translateX(100%)";
             arrayImagenes[indiceImagenActual-1].style.zIndex = "5";
-            indiceImagenActual++;
+            indiceImagenActual=0;
+            arrayImagenes[indiceImagenActual].style.transform = "translateX(0)"
+            arrayImagenes[indiceImagenActual].style.zIndex = "10";
+
+
         }
+    
+        setTimeout(function() {
+            transitionButton = false;
+        }, 700);
     }
-    else if (indiceImagenActual==(arrayImagenes.length-1)){
-        arrayImagenes[indiceImagenActual].style.transform = "translateX(-100%)";
-        arrayImagenes[indiceImagenActual].style.zIndex = "10";
-        arrayImagenes[indiceImagenActual-1].style.transform = "translateX(100%)";
-        arrayImagenes[indiceImagenActual-1].style.zIndex = "5";
-        indiceImagenActual=0;
-        arrayImagenes[indiceImagenActual].style.transform = "translateX(0)"
-        arrayImagenes[indiceImagenActual].style.zIndex = "10";
-
-
-    }
-
-    // clearInterval(intervaloCarrusel); 
-    // intervaloCarrusel = setInterval(siguienteImagen, 5000); 
+     clearInterval(intervaloCarrusel); 
+     intervaloCarrusel = setInterval(siguienteImagen, 5000); 
     //clearInterval para eliminar el intervalo generado con setInterval para que al presionar el boton se reinicie el temporizador de setInterval
 }
 
@@ -59,21 +76,44 @@ const flechaIzquierda= document.querySelector(".container-buton-left");
 flechaIzquierda.addEventListener("click",anteriorImagen);
 
 function anteriorImagen(){
-    if(indiceImagenActual !== 0){
-        arrayImagenes[indiceImagenActual-1].style.transform = "translateX(0)"
-        arrayImagenes[indiceImagenActual-1].style.zIndex = "10";
-        arrayImagenes[indiceImagenActual].style.transform = "translateX(100%)"
-        arrayImagenes[indiceImagenActual].style.zIndex = "auto";
-        indiceImagenActual = indiceImagenActual-1;
-    }
-    else{
-        arrayImagenes[indiceImagenActual].style.transform = "translateX(100%)"
-        arrayImagenes[indiceImagenActual].style.zIndex = "auto";
-        indiceImagenActual= (arrayImagenes.length-1);
-        arrayImagenes[indiceImagenActual].style.transform = "translateX(0)"
-        arrayImagenes[indiceImagenActual].style.zIndex = "10";
-    } 
+    if(!transitionButton){
+        transitionButton = true;
+        if(indiceImagenActual !== 0){ 
+            if(indiceImagenActual==arrayImagenes.length-1){ //para el maximo
+                arrayImagenes[indiceImagenActual-1].style.transform = "translateX(0)";
+                arrayImagenes[indiceImagenActual-1].style.zIndex = "10";
+                arrayImagenes[indiceImagenActual].style.transform = "translateX(100%)";
+                arrayImagenes[indiceImagenActual].style.zIndex = "10";
+                arrayImagenes[0].style.transform = "translateX(-100%)";
+                arrayImagenes[0].style.zIndex = "5";
+                indiceImagenActual= indiceImagenActual -1;
+            }
+            else{ //entre el 0 y el maximo
+                arrayImagenes[indiceImagenActual-1].style.transform = "translateX(0)";
+                arrayImagenes[indiceImagenActual-1].style.zIndex = "10";
+                arrayImagenes[indiceImagenActual].style.transform = "translateX(100%)";
+                arrayImagenes[indiceImagenActual].style.zIndex = "10";
+                arrayImagenes[indiceImagenActual+1].style.transform = "translateX(-100%)";
+                arrayImagenes[indiceImagenActual+1].style.zIndex = "5";
+                indiceImagenActual= indiceImagenActual -1;
+            }
+        }
+        else{ //para el 0
+            arrayImagenes[arrayImagenes.length-1].style.transform = "translateX(-100%)"
+            arrayImagenes[arrayImagenes.length-1].style.zIndex = "5";
+            arrayImagenes[indiceImagenActual].style.transform = "translateX(100%)";
+            arrayImagenes[indiceImagenActual].style.zIndex = "10";
+            indiceImagenActual=arrayImagenes.length-1;
+            arrayImagenes[indiceImagenActual].style.transform = "translateX(0)"
+            arrayImagenes[indiceImagenActual].style.zIndex = "10";
+            arrayImagenes[indiceImagenActual-1].style.transform = "translateX(-100%)";
+            arrayImagenes[indiceImagenActual-1].style.zIndex = "5";
 
-    // clearInterval(intervaloCarrusel);
-    // intervaloCarrusel = setInterval(siguienteImagen, 5000);    
+        }
+        setTimeout(function() {
+            transitionButton = false;
+        }, 700);
+    }
+    clearInterval(intervaloCarrusel);
+     intervaloCarrusel = setInterval(siguienteImagen, 5000);    
 }
